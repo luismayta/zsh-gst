3b3bbad3795aba8b5e7ec07b6d53b73a78da08ad
@slovacus <slovacus@gmail.com>
2019-12-30 03:21:15 -0500

    (#3) chore: implement settings for install gst

--- zsh-gst.zsh
+++ zsh-gst.zsh
@@ -7,30 +7,59 @@
 # Authors:
 #   Luis Mayta <slovacus@gmail.com>
 #
+# Links:
+#
+# gst: https://github.com/uetchy/gst
+#
+#
+#
 gst_package_name=gst
+GST_PATH_BIN=/usr/local/bin
+GST_BIN="${GST_PATH_BIN}/${gst_package_name}"
 
-function gst::dependences {
-    message_info "Installing dependences for $gst_package_name"
-    message_success "Installed dependences for $gst_package_name"
+function gst::install::osx {
+    curl -L https://github.com/uetchy/gst/releases/download/v5.0.1/gst_darwin_amd64 > "${GST_BIN}"
+    chmod +x "${GST_BIN}"
 }
 
-function gst::packages {
-    message_info "Install packages for $gst_package_name"
-    message_success "Installed packages for $gst_package_name"
+function gst::install::linux {
+    curl -L https://github.com/uetchy/gst/releases/download/v5.0.1/gst_linux_amd64 > "${GST_BIN}"
+    chmod +x "${GST_BIN}"
+}
+
+function gst::dependences::install {
+    messages_info "Installing Dependences for ${gst_package_name}"
+    messages_success "${gst_package_name} Dependences Installed"
+}
+
+function gst::dependences::checked {
+    if ! type -p curl > /dev/null; then
+      messages_error "Please install curl for  ${gst_package_name}"
+    fi
 }
 
 function gst::install {
-    gst::dependences
-    message_info "Installing $gst_package_name"
-    message_success "Installed $gst_package_name"
+    gst::dependences::checked
+    messages_info "Installing ${gst_package_name}"
+    case "${OSTYPE}" in
+    darwin*)
+        gst::install:osx
+        ;;
+    linux*)
+        gst::install::linux
+      ;;
+    esac
+    messages_success "${gst_package_name} Installed"
 }
 
 function gst::post_install {
     message_info "Post Install $gst_package_name"
-    message_success "Success Install $gst_package_name}"
+    message_success "Success Install $gst_package_name"
 }
 
+# load dependences
 function gst::load {
+    path_append "${GST_PATH_BIN}"
 }
 
 gst::load
